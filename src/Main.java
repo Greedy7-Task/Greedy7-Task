2ceba39de136ded6cb162f7cd95830531f03cc23
jongmine <joychoe21@icloud.com>
2021-04-03 20:49:27 +0900

    revise array

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
-package mst;
-//import mst.Graph;
-import java.util.Scanner;
+import java.util.*;
 import java.lang.*;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("정점의 개수를 입력하세요: ");
         int n = scan.nextInt();
-        int[][] G = new int[n][n]; // n = 점의 개수, m = 선분의 개수
+        int[][] G = new int[n][n];  // n = 점의 개수, m = 선분의 개수
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                G[i][j] = (int) (Math.random() * 10);
-                System.out.print(G[i][j]+" ");
-
+                int element = 0;
+                if (i == j)
+                    G[i][j] = element;
+                else {
+                    element = (int) (Math.random() * 9) + 1;
+                    G[i][j] = element;
+                    G[j][i] = element;
+                }
             }
-            System.out.println();
-
         }
 
 
