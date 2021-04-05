22372b1e7d8dfd5aa60dd1a07e0741c848a0f7b0
jongmine <joychoe21@icloud.com>
2021-04-05 21:14:02 +0900

    merged

--- src/Dijkstra.java
+++ src/Dijkstra.java
@@ -1,5 +1,4 @@
-package mst;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Dijkstra implements ShortestPath {
@@ -7,6 +6,40 @@ public class Dijkstra implements ShortestPath {
     int s = 0;
     private int a;
 
+    @Override
+    public int[] find_ShortestPath(int[][] G, int s) {
+        int vertex = G.length;
+        int edge = getEdgeCount(G);
+        int[] D = init(vertex, s);
+
+
+        return D;
+    }
+
+    public int[] init(int v, int s) {
+        int[] distance_array = new int[v];
+        Arrays.fill(distance_array, INF);
+        distance_array[s] = 0;
+        return distance_array;
+    }
+
+    public int getEdgeCount(int[][] G) {
+        int edge = 0;
+        for (int i = 0; i < G.length; i++)
+            if (G[0][i] != 0)
+                edge++;
+        return edge;
+    }
+
+    public void printDistance(int[] D, int s) {
+        System.out.printf("정점 %d부터의 거리", s);
+        System.out.println();
+        for (int i : D)
+            if (i != s) {
+                System.out.printf("%d번 점: %d", i, D[i]);
+                System.out.println();
+            }
+    }
 
     @Override
     public void convert(int[][] G) {
